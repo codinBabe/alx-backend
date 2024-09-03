74daa3f38c2b9ead3f29a97577933be1cfd8d473
codinBabe <teboola2@gmail.com>
2024-09-02 21:12:30 -0700

    fix:pycodestyle error

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,12 +4,13 @@
 
 from typing import Tuple
 
-def index_range(page: int, page_size: int)-> Tuple[int, int]:
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
-    A function that return a tuple of size two containing a start index 
-    and an end index corresponding to the range of indexes to return in a 
+    A function that return a tuple of size two containing a start index
+    and an end index corresponding to the range of indexes to return in a
     list for those particular pagination parameters.
     """
-    start_index = (page -1) * page_size
+    start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)

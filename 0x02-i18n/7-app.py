68e5d6ddd672ad40d9de45e9315bfcb5ae04726d
codinBabe <teboola2@gmail.com>
2024-09-13 19:38:14 +0100

    completed:task 7

---
+++ 0x02-i18n/7-app.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+"""A Basic Flask app"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+from typing import Union, Dict
+import pytz
+
+
+class Config:
+    """A config class representation"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user() -> Union[Dict, None]:
+    """return user dictionary"""
+    login_as = request.args.get('login_as')
+    if login_as:
+        return users.get(int(login_as))
+    return None
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """get the web page locale"""
+    locale = request.args.get('locale', '')
+    if locale in app.config["LANGUAGES"]:
+        return locale
+    if g.user and g.user['locale'] in app.config['LANGUAGES']:
+        return g.user['locale']
+    headers = request.headers.get('locale', '')
+    if headers in app.config['LANGUAGES']:
+        return headers
+    return app.config['BABEL_DEFAULT_LOCALE']
+
+
+@babel.timezoneselector
+def get_timzone() -> str:
+    """Get the web timezone"""
+    timezone = request.args.get('timezone', '').strip()
+    if not timezone and g.user:
+        timezone = g.user['timezone']
+    
+    try:
+       pytz.timezone(timezone).zone
+    except pytz.exceptions.UnknownTimeZoneError:
+        return app.config['BABEL_DEFAULT_TIMEZONE']
+    
+
+@app.route('/')
+def home() -> str:
+    """The home route"""
+    return render_template('6-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
